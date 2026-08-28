dbcc36b860ef398a3c0812b4c0365661e0d68059
galadmitro <galadmitro87392824@gmail.com>
2026-08-28 19:03:17 +0300

    Fix

---
+++ src/main/java/dev/leo/sableplayerragdoll/mob/MobRagdollAssembly.java
@@ -0,0 +1,46 @@
+package dev.leo.sableplayerragdoll.mob;
+
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.phys.Vec3;
+
+public class MobRagdollAssembly {
+    private final Mob mob;
+    private boolean assembled;
+
+    public MobRagdollAssembly(Mob mob) {
+        this.mob = mob;
+        this.assembled = false;
+    }
+
+    public static void assemble(Mob mob) {
+        if (mob == null || !mob.isAlive()) {
+            return;
+        }
+
+        MobRagdollAssembly assembly = new MobRagdollAssembly(mob);
+        assembly.buildRigidBodies();
+        assembly.applyJointConstraints();
+        assembly.assembled = true;
+    }
+
+    private void buildRigidBodies() {
+        Vec3 velocity = mob.getDeltaMovement();
+        double scale = mob.getAttributeValue(Attributes.SCALE);
+        mob.setDeltaMovement(velocity.x * scale, velocity.y, velocity.z * scale);
+        mob.setNoGravity(false);
+    }
+
+    private void applyJointConstraints() {
+        mob.hasImpulse = true;
+        mob.setOnGround(false);
+    }
+
+    public boolean isAssembled() {
+        return this.assembled;
+    }
+
+    public Mob getMob() {
+        return this.mob;
+    }
+}
--- src/main/java/dev/leo/sableplayerragdoll/mob/RenderedModelExtractor.java
+++ src/main/java/dev/leo/sableplayerragdoll/mob/RenderedModelExtractor.java
@@ -1,9 +1,32 @@
 package dev.leo.sableplayerragdoll.mob;
 
 import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.world.entity.Entity;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RenderedModelExtractor {
+    private static final Map<Entity, Map<String, ModelPart>> MODEL_CACHE = new HashMap<>();
+
     public static <T extends Entity> void extractModel(T entity, EntityModel<T> model) {
+        if (entity == null || model == null) {
+            return;
+        }
+
+        Map<String, ModelPart> partsMap = new HashMap<>();
+        model.root().getAllParts().forEach(part -> {
+            partsMap.put(part.toString(), part);
+        });
+
+        MODEL_CACHE.put(entity, partsMap);
+    }
+
+    public static Map<String, ModelPart> getExtractedParts(Entity entity) {
+        return MODEL_CACHE.getOrDefault(entity, Map.of());
+    }
+
+    public static void clearCache(Entity entity) {
+        MODEL_CACHE.remove(entity);
     }
 }
